30f44d99e34967877ff1c1015dc2997425482409
M Moazam <muhammadmoazzamshigri@gmail.com>
2024-03-31 17:22:26 +0500

    Uploaded

---
+++ app.js
@@ -0,0 +1,15 @@
+setInterval(() => {
+    d = new Date();
+    hourTime = d.getHours();
+    minuteTime = d.getMinutes();
+    secondsTime = d.getSeconds();
+    hourRotation = 30*hourTime + minuteTime/2;
+    minuteRotation = 6*minuteTime;
+    secondsRotation = 6*secondsTime;
+
+    hour.style.transform = `rotate(${hourRotation}deg)`;
+    minute.style.transform = `rotate(${minuteRotation}deg)`;
+    second.style.transform = `rotate(${secondsRotation}deg)`;
+    
+},1
+)
