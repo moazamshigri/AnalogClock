f8b14ddb0174aa07c5832591232db89540f44caa
M Moazam <muhammadmoazzamshigri@gmail.com>
2024-04-01 12:23:15 +0500

    uploaded

--- app.js
+++ app.js
@@ -16,3 +16,22 @@ setInterval(() => {
 
 let mode = document.querySelector(".custom-control-input");
 
+let modeLabel = document.querySelector(".modeLabel");
+modeLabel.innerHTML = "Light Mode";
+var styleSheet = document.getElementById('styleSheet');
+let flag = 0;
+mode.addEventListener("click",() => {
+    flag+=1;
+    if(flag%2 == 0){
+        modeLabel.innerHTML = "Light Mode";
+
+        styleSheet.setAttribute('href', 'style.css');
+    }
+    else{
+        modeLabel.innerHTML = "Dark Mode";
+        styleSheet.setAttribute('href', 'style2.css');
+        
+    }
+
+});
+
